c244869f4fe801b4c1fa054810b9ce925b54cca6
Glenn Davy <glenn@davy.net.au>
2008-09-09 17:48:37 +1000

    rdoc for nested set

--- app/models/outgoing_category.rb
+++ app/models/outgoing_category.rb
@@ -2,4 +2,9 @@ class OutgoingCategory < ActiveRecord::Base
   has_many :outgoing_category_allocations
   has_many :flushes, :through => :outgoing_category_allocations 
   validates_presence_of :name
+
+  acts_as_nested_set
+
+  
+
 end
