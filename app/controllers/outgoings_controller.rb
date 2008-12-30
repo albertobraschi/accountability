ee287d8298755493eb6ab99afcd456bbc5e0cb45
Glenn Davy <glenn@balder.(none)>
2008-12-30 17:19:20 +1100

    allow transaction to be assigned to a statement

--- app/controllers/outgoings_controller.rb
+++ app/controllers/outgoings_controller.rb
@@ -4,5 +4,8 @@ class OutgoingsController < ApplicationController
     before(:edit, :new) do
       @accounts = Account.find(:all).collect{|c| [c.title, c.id]}
     end
+    before(:edit, :show, :update) do
+      @account = Account.find(@incoming.account_id)
+    end
   end
 end
